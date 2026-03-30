8a1577cadf314b8d12517d6a0ccddf1b6a20ab44
PRIYANKA-GADAM <priyankayadavgpy5@gmail.com>
2026-03-30 11:53:11 +0530

    Change S3 bucket name for Terraform backend

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "priya-project-s3-bucket"
+    bucket         = "priya-project1-s3-bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "mc-eks-state-lock"
